aaf25418a59dceeb0bc878baa7c7bb9cc0d0b88a
Jintin <jintin.lin1018@gmail.com>
2020-12-02 13:35:16 +0800

    init

---
+++ app/src/main/java/com/jintin/retrofitsample/GithubService.kt
@@ -0,0 +1,16 @@
+package com.jintin.retrofitsample
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GitHubService {
+    @GET("users/{user}/repos")
+    fun listRepos(
+        @Path("user") user: String,
+        @Query("type") type: String? = null,
+        @Query("sort") sort: String? = null
+    ): Call<List<Repo>>
+    
+}
